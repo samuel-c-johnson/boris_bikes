651ae7db7da4567cba8000c242a91055b53a202c
charles <charlesemery15@gmail.com>
2017-11-21 17:42:33 +0000

    Step_10 Making Docking Stations Relsease Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,10 @@
 require "DockingStation"
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
+
+it { is_expected.to respond_to :release_bike }
+  it "releases a working bike" do
+    bike = subject.release_bike
+    expect(bike.working?).to be(true)
+  end
 end
