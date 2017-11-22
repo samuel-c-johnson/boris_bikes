8de30a90dc8e99a271a91bd7fb1f4113d25cdc37
charles <charlesemery15@gmail.com>
2017-11-22 18:49:06 +0000

    Step_15 private full/empty

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -8,15 +8,24 @@ class DockingStation
   end
 
   def release_bike
-    raise RuntimeError, "No bikes available" unless bikes.length >=1 #best practice as attr_reader adds '@'
+    raise RuntimeError, "No bikes available" unless !empty? #best practice as attr_reader adds '@'
     @bikes.pop
   end
 
   def dock(a_bike)
     # should would 19 or less
-    raise RuntimeError, "Dock full" unless bikes.length <= 19  #guard condition
+    raise RuntimeError, "Dock full" unless !full?  #guard condition
     # adding bikes to the "DockingStation"
     @bikes.push(a_bike)
+  end
+  private
+
+  def full?
+    if bikes.length == 20 then true else false end
+  end
+
+  def empty?
+    if bikes.length == 0 then true else false end
   end
 
 end
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/DockingStation.rb"
 require_relative "../lib/Bike.rb"
 station = DockingStation.new
-20.times{ station.dock(Bike.new) }
+19.times{ station.dock(Bike.new) }
 #station.release_bike
-21.times{ station.release_bike}
-p station
+#20.times{ station.release_bike}
+p station.full?
