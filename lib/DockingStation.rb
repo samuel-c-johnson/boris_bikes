deb76aa5f02189b98661c84161dd4ad1f7d1d887
charles <charlesemery15@gmail.com>
2017-11-22 17:01:00 +0000

    Step_12 Bike availability check

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -4,15 +4,8 @@ class DockingStation
   attr_reader :bike #don't need to initiliaze the bike instance variable
 
   def release_bike
-    raise RuntimeError.new
+    raise RuntimeError, "No bikes available" unless @bike
     @bike
-    # if bike # if there is a docked bike
-    #   a_bike = @bike
-    #   @bike = nil #have to say that as the bike is returned (released) there will be no bike left before releasing said bike
-    #   return a_bike
-    # else
-    #   raise RuntimeError.new()
-    # end
   end
 
   def dock(a_bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,17 +3,30 @@ require "Bike.rb"
 
 describe DockingStation do
 
-  it "releases a bike" do
-    is_expected.to respond_to :release_bike #whenever refereing to a method inside rspec use symbol eg//: :release_bike not release_bike
+  it { is_expected.to respond_to :release_bike }
+  #another way of writing the test v
+    # it "releases a bike" do
+    #   is_expected.to respond_to :release_bike #whenever refereing to a method inside rspec use symbol eg//: :release_bike not release_bike
+    # end
+  it "releases working bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    bike = subject.release_bike # setting up test ^
+    expect(bike).to be_working # test
   end
-  it "released bike, is working" do
-    bike = subject.release_bike
-    expect(bike.working?).to be(true)
-  end
-  it "dock has accepted an argument" do
-    expect(subject).to respond_to(:dock).with(1).argument
-  end
-  it "should raise error when no bike docked" do #if no bike is docked and you try to release a bike an error is raised
-    expect(subject.release_bike).to raise_error(RuntimeError)
+      it "dock has accepted an argument" do
+        expect(subject).to respond_to(:dock).with(1).argument
+      end
+
+  describe "#release_bike" do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike  #why does it have curly bs v and this doesn't
+    end
+
+    it "should raise error when no bike docked" do #if no bike is docked and you try to release a bike an error is raised
+      expect{ subject.release_bike }.to raise_error(RuntimeError, "No bikes available")
+    end
   end
 end
